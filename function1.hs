6cc5c105d8a548d366948b412d61b631eccb46e7
abdikeem <abdihakimabdirahman21@gmail.com>
2022-03-31 16:35:43 +0300

    function one

---
+++ function1.hs
@@ -0,0 +1,15 @@
+import Data.List
+import System.IO
+
+-- funcName para1 param2 = operations (returned value)
+
+addMe x y = x + y 
+{-
+commands: 
+:ghci
+ :l {name_of_file}
+ :r
+ :t {funcName}
+ RUN: {funcName} param1 param2
+
+-}
