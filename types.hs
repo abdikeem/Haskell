903237be5fc686aed0c6cfd439ff6b8fbb661555
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-03 03:57:05 +0300

    types

---
+++ types.hs
@@ -0,0 +1,10 @@
+{-
+Topic: Types and typeclasses
+We can use :type or :t to find out the type of a value
+eg:
+ghci> :t "kimza" // OUTPUT = "kimza" :: String
+
+NB: While using Double quotes("") the output will be string and single quotes('') the output is char in single letter.
+
+
+-}
