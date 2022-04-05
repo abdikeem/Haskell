8ed416790f2a81f87efd88697800c785d38c4969
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-06 01:55:57 +0300

    function

--- function2.hs
+++ function2.hs
@@ -1,11 +1,17 @@
 {-
 This is a recursive function. It is defined it terms of itself.
 -}
-increasing :: (Ord a) => [a] -> Bool
+{- increasing :: (Ord a) => [a] -> Bool
 increasing xs = if xs == []
     then True
     else if tail xs == []
         then True
         else if head xs <= head (tail xs)
             then increasing (tail xs)
-            else False
+            else False -}
+
+-- Haskell format
+increasing :: (Ord a) => [a] -> Bool
+increasing [] = True
+increasing[x] = True
+increasing (x:y:ys) = x <= y && increasing (y:ys)
